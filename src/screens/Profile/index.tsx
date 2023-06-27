4ea4e0fe6f54d74b2b126c34b2bf008664aea033
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2023-06-28 01:13:27 +0530

    Add delete btn in profile and update contacts screen view.

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -6,9 +6,15 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import {
+  responsiveHeight,
+  responsiveWidth,
+} from 'react-native-responsive-dimensions';
 import chevronRightIcon from '../../assets/images/chevron-right.png';
 import emailIcon from '../../assets/images/email-dark.png';
+import deleteIcon from '../../assets/images/trash.png';
 import userIcon from '../../assets/images/user.png';
+import {percentToPx} from '../../constants';
 import textStyles from '../../constants/fonts';
 import {useAuth} from '../../hooks/useAuth';
 import {flushStorage} from '../../lib/storage';
@@ -27,14 +33,21 @@ const ProfileScreen = () => {
   };
 
   return (
-    <View className="px-10 py-5 bg-white h-screen">
-      <Text
-        style={textStyles.bebasNeueBold}
-        className="text-dark-blue text-4xl">
-        PROFILE
-      </Text>
-      <View className="mt-[66px]">
-        <View>
+    <View
+      className="bg-white flex justify-between"
+      style={{
+        height: '100%',
+        paddingLeft: responsiveHeight(36 / percentToPx),
+        paddingRight: responsiveHeight(40 / percentToPx),
+        paddingVertical: responsiveHeight(20 / percentToPx),
+      }}>
+      <View>
+        <Text
+          style={textStyles.bebasNeueBold}
+          className="text-dark-blue text-4xl">
+          PROFILE
+        </Text>
+        <View style={{marginTop: responsiveHeight(66 / percentToPx)}}>
           <View>
             <Text
               style={textStyles.robotoRegular}
@@ -54,9 +67,7 @@ const ProfileScreen = () => {
             </View>
             <View className="h-[1px] w-full bg-off-white-2 mt-3" />
           </View>
-        </View>
-        <View className="mt-[10px]">
-          <View>
+          <View className="mt-[10px]">
             <Text
               style={textStyles.robotoRegular}
               className="text-dark-blue text-base">
@@ -76,24 +87,45 @@ const ProfileScreen = () => {
             </View>
             <View className="h-[1px] w-full bg-off-white-2 mt-3" />
           </View>
-        </View>
-        <View className="mt-[10px]">
-          <TouchableOpacity
-            onPress={handleLogout}
-            className="flex flex-row mt-6 items-center justify-between">
-            <Text
-              style={textStyles.robotoRegular}
-              className="text-dark-blue text-base">
-              Logout
-            </Text>
-            <Image
-              resizeMode="contain"
-              className="w-[10px] h-[12px]"
-              source={chevronRightIcon as ImageSourcePropType}
-            />
-          </TouchableOpacity>
+          <View className="mt-[10px]">
+            <TouchableOpacity
+              onPress={handleLogout}
+              className="flex flex-row mt-6 items-center justify-between">
+              <Text
+                style={textStyles.robotoRegular}
+                className="text-dark-blue text-base">
+                Logout
+              </Text>
+              <Image
+                resizeMode="contain"
+                className="w-[10px] h-[12px]"
+                source={chevronRightIcon as ImageSourcePropType}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
+      <TouchableOpacity
+        activeOpacity={0.5}
+        className="flex flex-row justify-center items-center border-[1px] border-accent rounded-lg"
+        style={{
+          paddingVertical: responsiveHeight(10 / percentToPx),
+        }}>
+        <>
+          <Image
+            resizeMode="center"
+            source={deleteIcon as ImageSourcePropType}
+            style={{
+              width: responsiveWidth(24 / percentToPx),
+              height: responsiveHeight(16 / percentToPx),
+              marginRight: responsiveHeight(10 / percentToPx),
+            }}
+          />
+          <Text className="text-accent" style={textStyles.robotoMedium}>
+            Delete Account
+          </Text>
+        </>
+      </TouchableOpacity>
     </View>
   );
 };
