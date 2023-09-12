c4c230d24fad855318f751017b902992c8f992fb
hashaam <hashaamkhan4247@gmail.com>
2023-09-12 21:48:01 +0500

    UIfix: Social Screens

--- src/screens/SocialLinks/WhatsApp/index.tsx
+++ src/screens/SocialLinks/WhatsApp/index.tsx
@@ -1,6 +1,6 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useEffect, useState} from 'react';
-import {Pressable, Text, View} from 'react-native';
+import {Pressable, Text, View, ScrollView} from 'react-native';
 import {CountryPicker} from 'react-native-country-codes-picker';
 import {ChevronDownIcon} from 'react-native-heroicons/outline';
 
@@ -15,6 +15,8 @@ import {useCreateBusinessCard} from '../../../hooks/useBusinessCard';
 import {ISocialLink} from '../../../hooks/useBusinessCard/interface';
 import Toast from '../../../lib/toast';
 import {AppStackParams} from '../../../navigation/AppNavigation';
+import StaticContainerReg from '../../../containers/StaticContainerReg';
+import GenericTextField from '../../../components/TextField/GenericTextField/GenericTextField';
 
 export type SocialLinksProps = NativeStackScreenProps<
   AppStackParams,
@@ -33,8 +35,8 @@ const WhatsAppScreen = ({navigation, route: {params}}: SocialLinksProps) => {
   });
 
   const [data, setData] = useState<{flag: string; code: string}>({
-    flag: '🇮🇳',
-    code: '+91',
+    flag: '🇺🇸',
+    code: '+1',
   });
 
   const handleSave = () => {
@@ -86,68 +88,80 @@ const WhatsAppScreen = ({navigation, route: {params}}: SocialLinksProps) => {
           }}
         />
       )}
-      <View
-        style={{
-          paddingVertical: responsiveHeight(32 / percentToPx),
-          paddingHorizontal: responsiveHeight(40 / percentToPx),
+      <StaticContainerReg
+        isBack
+        isHeader
+        title="Add other socials"
+        onBackPress={() => {
+          navigation.navigate('SocialLinksScreen', {cardId, status});
         }}>
-        <HeaderStepCount
-          showDotes={false}
-          onBackPress={() =>
-            navigation.navigate('SocialLinksScreen', {cardId, status})
-          }
-        />
-        <View
-          style={{
-            marginTop: responsiveHeight(20 / percentToPx),
-            marginBottom: responsiveHeight(22 / percentToPx),
+        <ScrollView
+          className="h-screen w-full"
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingVertical: responsiveHeight(17 / percentToPx),
+            paddingHorizontal: responsiveHeight(4 / percentToPx),
           }}>
-          <HeaderWithText
-            heading="ADD WHATSAPP"
-            subtitle="Please fill the following detail."
-          />
-        </View>
-        <View
-          className="relative"
-          style={{marginBottom: responsiveHeight(10 / percentToPx)}}>
-          <TextField
-            label="WhatsApp"
-            value={social.url}
-            style={{paddingLeft: 75}}
-            keyboardType="number-pad"
-            placeholder="WhatsApp number"
-            onChangeText={url => setSocial(state => ({...state, url}))}
-          />
-          {data.flag && (
+          <View className="bg-secondary-blue rounded-3xl p-4">
             <View
-              className="absolute"
               style={{
-                top: responsiveHeight(3.7),
-                left: responsiveHeight(1.3),
+                marginTop: responsiveHeight(12 / percentToPx),
+                marginBottom: responsiveHeight(22 / percentToPx),
               }}>
-              <Pressable
-                className="flex flex-row items-center gap-1 justify-center"
-                onPress={() => setOpen(true)}>
-                <View className="flex flex-row items-center gap-1 justify-center">
-                  <Text className="text-[24px]">{data.flag}</Text>
-                  <View className="top-[1.5px]">
-                    <ChevronDownIcon size={20} color="black" />
-                  </View>
-                </View>
-              </Pressable>
+              <View className="w-full">
+                <Text className="text-xl font-3 text-black text-center">
+                  Add whatsapp account number
+                </Text>
+              </View>
             </View>
-          )}
-        </View>
-        <TextField
-          label="Title"
-          value={social.title}
-          placeholder="Whatsapp"
-          onChangeText={title => setSocial(state => ({...state, title}))}
-        />
-        <View style={{marginTop: responsiveHeight(52 / percentToPx)}}>
-          <Button text="Save" callback={handleSave} />
-        </View>
-      </View>
+            <View
+              className="relative"
+              style={{marginBottom: responsiveHeight(10 / percentToPx)}}>
+              <TextField
+                label="WhatsApp"
+                value={social.url}
+                style={{
+                  paddingLeft: 75,
+                  backgroundColor: '#fff',
+                  borderRadius: 10,
+                  borderWidth: 0,
+                }}
+                keyboardType="number-pad"
+                placeholder="WhatsApp number"
+                onChangeText={url => setSocial(state => ({...state, url}))}
+              />
+              {data.flag && (
+                <View
+                  className="absolute"
+                  style={{
+                    top: responsiveHeight(4),
+                    left: responsiveHeight(1.3),
+                  }}>
+                  <Pressable
+                    className="flex flex-row items-center gap-1 justify-center"
+                    onPress={() => setOpen(true)}>
+                    <View className="flex flex-row items-center gap-1 justify-center">
+                      <Text className="text-[24px]">{data.flag}</Text>
+                      <View className="top-[1.5px]">
+                        <ChevronDownIcon size={20} color="black" />
+                      </View>
+                    </View>
+                  </Pressable>
+                </View>
+              )}
+            </View>
+            <GenericTextField
+              // label="Title"
+              value={social.title}
+              placeholder="Whatsapp"
+              onChangeText={title => setSocial(state => ({...state, title}))}
+            />
+            <View style={{marginTop: responsiveHeight(20 / percentToPx)}}>
+              <Button text="Save" callback={handleSave} />
+            </View>
+          </View>
+        </ScrollView>
+      </StaticContainerReg>
     </Layout>
   );
 };
